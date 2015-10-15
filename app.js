ea1f86ae651d1529c68762c978ebe7983e91a04c
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-15 23:12:28 +0200

    read dvd releases

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bootstrapMysql = require('./bootstrap/mysql');
 var port = process.env.PORT || 1337;
 
 app = module.exports = express();
-bootstrapMysql(config.mysql);
+app.db = bootstrapMysql(config.mysql);
 
 require('./lib/list/moviestart')(app);
 
--- bootstrap/mysql.js
+++ bootstrap/mysql.js
@@ -2,18 +2,16 @@ var mysql = require('mysql');
 var connection = null;
 
 module.exports = function (config) {
-	if (!connection) {
-		connection = mysql.createConnection(config);
-		connection.connect(function (error) {
-			if (error) {
-				console.log('Error connecting to MySQL on ' + config.host);
-				console.log(error);
-				return;
-			}
+	connection = mysql.createConnection(config);
+	connection.connect(function (error) {
+		if (error) {
+			console.log('Error connecting to MySQL on ' + config.host);
+			console.log(error);
+			return;
+		}
 
-			console.log('Connected to MySQL on ' + config.host);
-		});
-	}
+		console.log('Connected to MySQL on ' + config.host);
+	});
 
 	return connection;
 };
