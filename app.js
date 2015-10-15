a9bf8caf3af81ecce54380b48636371f5433aeb7
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-15 21:25:49 +0200

    start the server

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var express = require('express');
+var port = process.env.PORT || 1337;
 
-app = module.exports = express()
+app = module.exports = express();
+
+app.listen(port, function () {
+	console.log('Server listening on port ' + port);
+});
