845d8e4cf238d1bbf5b35aa678abb2504b73f7b5
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-15 22:11:43 +0200

    create mysql db

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var config = require('iniparser').parseSync('./config.ini');
 var express = require('express');
 var port = process.env.PORT || 1337;
+var db = require('./lib/db');
 
 app = module.exports = express();
+db.connect(config.mysql);
 
 app.listen(port, function () {
 	console.log('Server listening on port ' + port);
